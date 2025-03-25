36aa704246230eb0f38304ca19b78aabb3af45c6
muradhani <mouradhany574@gmail.com>
2025-03-25 16:18:02 +0200

    import domain into data module

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.gson)
+    implementation(project(":domain"))
 }
